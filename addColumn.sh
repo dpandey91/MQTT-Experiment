961f3df3ce6968fd3b784206cae8cb3d522640df
Dipika Pandey <dipika.pandey@wct-inc.com>
2016-12-05 04:44:07 -0500

    Added script for combining colm

---
+++ addColumn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while read compareFile1 <&3 && read compareFile2 <&4; do     
+    
+   #echo "Comparing file - $compareFile"
+
+#if diff "$compareFile1" "$compareFile2" >/dev/null ; then
+ 
+#   echo Same
+ #else
+      echo $compareFile1  $compareFile2 >> output.txt
+#echo $compareFile2
+
+ #fi 
+done 3<cOut.txt 4<pOut.txt
